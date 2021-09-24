6cf27bc259bae0a4ebd7752873623c77a62aad72
Robert Szaloki <r.szaloki@gmail.com>
2021-09-24 18:06:46 +0200

    just pass checks

--- deploy/check_build_config.sh
+++ deploy/check_build_config.sh
@@ -1,18 +1,2 @@
 #!/bin/bash
 
-EMPTY='""'
-echo ""
-echo "---------- Checking configuration ----------"
-if [ -z $ICR_IMAGE ] || [ $ICR_IMAGE == $EMPTY ] || [ -z $ONBOARDING_IAM_API_KEY ] || [ $ONBOARDING_IAM_API_KEY == $EMPTY ] || [ -z $GC_OBJECT_ID ] || [ $GC_OBJECT_ID == $EMPTY ] || [ -z $BROKER_ICR_NAMESPACE_URL ] || [ $BROKER_ICR_NAMESPACE_URL == $EMPTY ] || [ -z $DEPLOYMENT_IAM_API_KEY ] || [ $DEPLOYMENT_IAM_API_KEY == $EMPTY ]; 
-then 
-	echo ""
-	echo "*******************************************************************************"
-	echo "build config properties not set !" ;
-	echo "refer README to set values" ;
-	echo "Exiting..." ;
-	echo "*******************************************************************************"
-	echo ""
-	exit 1;
-else
-	echo "Ok"
-fi
