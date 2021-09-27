7bd8cdb60d8c0834f05fe1b6d41929fd358e0ac3
Robert Szaloki <robert.szaloki@ibm.com>
2021-09-27 16:49:07 +0200

    check secrets independently

--- deploy/check_build_config.sh
+++ deploy/check_build_config.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 CONFIG_FILE=deploy/build.config.properties
-DEPLOYMENT_IAM_API_KEY=`cat ${CONFIG_FILE} | grep DEPLOYMENT_IAM_API_KEY | cut -d'=' -f2`
 BROKER_ICR_NAMESPACE_URL=`cat ${CONFIG_FILE} | grep BROKER_ICR_NAMESPACE_URL | cut -d'=' -f2`
 GC_OBJECT_ID=`cat ${CONFIG_FILE} | grep GC_OBJECT_ID | cut -d'=' -f2`
-ONBOARDING_IAM_API_KEY=`cat ${CONFIG_FILE} | grep ONBOARDING_IAM_API_KEY | cut -d'=' -f2`
 ICR_IMAGE=`cat ${CONFIG_FILE} | grep ICR_IMAGE | cut -d'=' -f2`
 ONBOARDING_ENV=`cat ${CONFIG_FILE} | grep ONBOARDING_ENV | cut -d'=' -f2`
+ICR_NAMESPACE_REGION=`cat ${CONFIG_FILE} | grep ICR_NAMESPACE_REGION | cut -d'=' -f2`
 
 EMPTY='""'
 echo ""
@@ -16,6 +15,22 @@ then
 	echo ""
 	echo "*******************************************************************************"
 	echo "build config properties not set !" ;
+	echo "refer README to set values" ;
+	echo "Exiting..." ;
+	echo "*******************************************************************************"
+	echo ""
+	exit 1;
+else
+	echo "Ok"
+fi
+
+echo ""
+echo "---------- Checking secrets ----------"
+if [ -z $DEPLOYMENT_IAM_API_KEY ] || [ $DEPLOYMENT_IAM_API_KEY == $EMPTY ] || [ -z $ONBOARDING_IAM_API_KEY ] || [ $ONBOARDING_IAM_API_KEY == $EMPTY ];
+then
+	echo ""
+	echo "*******************************************************************************"
+	echo "secrets not set !" ;
 	echo "refer README to set values" ;
 	echo "Exiting..." ;
 	echo "*******************************************************************************"
